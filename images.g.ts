f2cd9476e45cf92b400602a0aa0a89cd6b4c79c1
1093016 <160205543+1093016@users.noreply.github.com>
2024-02-21 08:30:50 +0800

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,27 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song1":
+            case "character selection":return hex`0078000408010207001c00020a006400f401640000040000000000000000000000000000000003300008000900011d0a000b0001200c000d0001240e000f0001271800190001191a001b00011d1c001d0001201e001f00012408001c000e050046006603320000040a002d00000064001400010000020000020c0000000400010c100014000108`;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
